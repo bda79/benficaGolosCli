53b0f5cd3ec8b0ceca2cdafb4ef4eb8358f652a4
Bruno Aleluia <bruno_aleluia@hotmail.com>
2020-09-21 16:49:47 +0100

    add spinner when heroku skeeps app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.scss';
 import { AllRoutes } from './routes/allRoutes';
+import * as isHerokuSleeping from 'react-use-heroku';
+import loginImg from '../src/img/Slb-spinner.gif';
+import {server_url} from './service/config';
+
+const url = `${server_url}/wake-up`;
 
 function App() {
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV !== 'development' && isHerokuSleeping({ url })) return ( 
+    <div id="loader-wu">
+      <div className="loader">
+        <img src={loginImg} alt="" />
+      </div>
+      <div>Waking Up App...</div>
+    </div> )
+ 
   return (
     <AllRoutes />
   );

d72b35f228ba79a1dc693d7ceeded41faa2cee7a
Bruno Aleluia <bruno_aleluia@hotmail.com>
2020-09-21 18:35:10 +0100

    fix wakeup heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import { AllRoutes } from './routes/allRoutes';
-import * as isHerokuSleeping from 'react-use-heroku';
 import loginImg from '../src/img/Slb-spinner.gif';
-import {server_url} from './service/config';
+import ServiceData from "../src/service/dataUtils";
 
-const url = `${server_url}/wake-up`;
+const App = () => {
+  const [ loading, setLoading ] = useState(true);
 
-function App() {
-  console.log(process.env.NODE_ENV)
-  if (process.env.NODE_ENV !== 'development' && isHerokuSleeping({ url })) return ( 
-    <div id="loader-wu">
-      <div className="loader">
-        <img src={loginImg} alt="" />
+  const awake = async () => {
+    const headers = ServiceData.headers();
+    const options = ServiceData.options('GET', null, headers);
+    await ServiceData.execute('wake-up', options)
+        .then((res) => {
+            if (res.data)
+              setLoading(false);
+            
+        })
+        .catch(err => {
+            console.log(err);
+        });
+  };
+
+  useEffect(() => {
+    awake();
+  }, []);
+
+  if (loading) {
+     return (
+      <div id="loader-wu">
+        <div className="loader">
+          <img src={loginImg} alt="" />
+        </div>
+        <div>Waking Up App...</div>
       </div>
-      <div>Waking Up App...</div>
-    </div> )
- 
-  return (
-    <AllRoutes />
-  );
-}
+    )
+  }
+  else {
+   return (
+      <AllRoutes />
+    );
+  }
+};
 
 export default App;
